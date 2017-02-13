214715752e3564df41a3b596838ddffaac81a878
Marcus Roy <mdroy@us.ibm.com>
2017-02-13 10:50:09 -0600

    Creating VM AirWatch plugin for UCD.

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
-# IBM UrbanCode Deploy Plug-in Template
+# IBM UrbanCode Deploy AirWatch Plugin
 ---
 
-The UrbanCode Deploy Plug-in Template project is a sample project that can be forked and used as a base for community plug-ins.
+### Compatibility
+	The IBM UrbanCode Deploy automation plugin uses AirWatch REST API. This plugin has been built and tested against AirwWatch 8.4.
+	This plug-in requires version 6.1.1 or later of IBM UrbanCode Deploy.
 
-Create your own plug-in by following our [beginner](https://developer.ibm.com/urbancode/docs/urbancode-plug-development-tutorial/) and [upgrade](https://developer.ibm.com/urbancode/docs/community-plug-development-upgrade-tutorial/) tutorials. For more documentation on plug-in development, see the [IBM UrbanCode Build help](http://www-01.ibm.com/support/knowledgecenter/SS8NMD_6.1.2/com.ibm.ucbuild.doc/topics/c_node_extend.html) or the [IBM UrbanCode Deploy help](http://www.ibm.com/support/knowledgecenter/SS4GSP_6.2.1/com.ibm.udeploy.doc/topics/c_node_extending.html).
+### Installation
 
-### Compiling Source
-`gradle`
+	The packaged zip is located in the build/distributions folder. No special steps are required for installation.
+	See Installing plug-ins in UrbanCode Deploy. Download this zip file if you wish to skip the
+	manual build step. Otherwise, download the entire Slack-UCD and
+	run the "gradle" command in the top level folder. This should compile the code and create
+	a new distributable zip within the build/distributions folder. Use this command if you wish to make
+	your own changes to the plugin.
 
-The helloworld plug-in .zip file can be found in build/distributions after you build the project.
+### History
 
-### License
-This plugin is protected under the [Eclipse Public 1.0 License](http://www.eclipse.org/legal/epl-v10.html)
+    Version 1
+        Created initial version of VM AirWatch Urban Code Deployment plugin.
+
+### How to build the plugin from command line:
+
+1. Navigate to the base folder of the project through command line.
+2. Make sure that there is build.gradle file there, and then execute 'gradle' command.
+3. The built plugin is located at build/distributions/airwatch-ucd-1.dev.zip
+
+Note: Edit the gradle.properties to change the version 'apiVersion' to a release number.
---
+++ src/main/groovy/airwatch-ucd.groovy
@@ -0,0 +1,214 @@
+import com.urbancode.air.AirPluginTool;
+import groovyx.net.http.ContentType
+import groovyx.net.http.HTTPBuilder
+import static groovyx.net.http.Method.POST
+import static groovyx.net.http.Method.DELETE
+
+class AirWatchClient {
+
+    def baseUrl
+    def authToken
+    def basicAuth
+
+    AirWatchClient(baseUrl, authToken, userName, password) {
+        this.baseUrl = baseUrl
+        this.authToken = authToken
+        def base64BasicAuth = "${userName}:${password}".getBytes().encodeBase64().toString()
+        this.basicAuth =  "Basic ${base64BasicAuth}"
+    }
+
+    def removeIpa(String applicationId) {
+        deactiveApplication(applicationId)
+        deleteApplication(applicationId)
+    }
+
+    def uploadIpa(String fileName, String applicationName, smartGroup) {
+        def transactionId = uploadApplicationChunks(fileName)
+        def applicationId = saveRecord(transactionId, applicationName)
+        removeIpa(applicationId)
+    }
+
+    def uploadApplicationChunks(fileName) {
+
+        def url = baseUrl + "api/mam/apps/internal/uploadchunk"
+
+        def partCounter = 1
+        def sizeOfFiles = 1024 * 1024
+        def buffer =  new byte[sizeOfFiles]
+        File inputFile = new File(fileName)
+        BufferedInputStream bis = new BufferedInputStream(
+                new FileInputStream(fileName))
+        int tmp = 0
+
+        def numberOfChunks = ((inputFile.length() / sizeOfFiles) + 1).intValue()
+
+        println "Uploading file: " + inputFile.name + " split into " + numberOfChunks + " chunk/s"
+    
+        String transactionId = null
+        while ((tmp = bis.read(buffer)) > 0) {
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream()
+            out.write(buffer, 0, tmp)
+            out.close()
+
+            String encoded = out.toByteArray().encodeBase64().toString()
+
+            HTTPBuilder http = new HTTPBuilder(url)
+
+            http.request(POST) {
+
+                body = [ChunkSize: tmp, TotalApplicationSize: inputFile.length(),
+                        ChunkSequenceNumber: partCounter, ChunkData: encoded,TransactionId: transactionId]
+
+                requestContentType = ContentType.JSON
+
+                headers.'aw-tenant-code' = authToken
+                headers.'Authorization' = basicAuth
+
+                response.success = { resp, json ->
+                    transactionId = json["TranscationId"]
+                    println "Success: Uploaded chunk: " + partCounter + " transactionId: " + transactionId
+                }
+
+                response.failure = { resp, json ->
+                    println "Error: Failed to uploaded chunk:" + partCounter
+                    println "Error: Raw JSON Response: " + json
+
+                    System.exit(1)
+                }
+            }
+            partCounter++
+        }
+        return transactionId
+    }
+
+    def saveRecord(String transactionId, String applicationName) {
+        
+        def applicationId
+
+        def url = baseUrl + "api/mam/apps/internal/begininstall"
+        HTTPBuilder http = new HTTPBuilder(url)
+
+        http.request(POST) {
+
+            body = [TransactionId: transactionId,
+                    AutoUpdateVersion: true,
+                    DeviceType: "2",
+                    ApplicationName: applicationName,
+                    PushMode: "OnDemand",
+                    SupportedModels: [Model: [[ModelId : 1, ModelName:"iPhone"], [ModelId : 2, ModelName:"iPad"]]]]
+
+            requestContentType = ContentType.JSON
+
+            headers.'aw-tenant-code' = authToken
+            headers.'Authorization' = basicAuth
+
+            response.success = { resp, json ->
+
+                applicationId = "${json["Id"]["Value"]}"
+
+                println "Success: Saved application" + applicationName + " with transactionId: " + transactionId
+            }
+
+            response.failure = { resp, json ->
+                println "Error: Failed to save application " + applicationName
+                println "Error: Raw JSON Response: " + json
+                System.exit(1)
+            }
+        }
+
+        return applicationId
+    }
+
+    def assignGroup(applicationId, smartGroup) {
+
+        def url = baseUrl + 'api/mam/apps/internal/' + applicationId + '/smartgroups/' + smartGroup
+
+        HTTPBuilder http = new HTTPBuilder(url)
+        http.request(POST) {
+
+            headers.'aw-tenant-code' = authToken
+            headers.'Authorization' = basicAuth
+
+            requestContentType = ContentType.JSON
+
+            response.success = { resp, json ->
+                println "Success: Assigned application: " + applicationId + " to group " + smartGroup
+            }
+
+            response.failure = { resp, json ->
+                println "Error: Unable to assign IPA to group"
+                println "Error: Raw JSON Response: " + json
+
+                System.exit(1)
+            }
+        }
+    }
+
+    def deactiveApplication(applicationId) {
+
+        def url = baseUrl + 'api/mam/apps/internal/' + applicationId + '/Deactivate'
+
+        HTTPBuilder http = new HTTPBuilder(url)
+        http.request(POST) {
+
+            headers.'aw-tenant-code' = authToken
+            headers.'Authorization' = basicAuth
+
+            requestContentType = ContentType.JSON
+
+            response.success = { resp, json ->
+                println "Success: Deactived application: " + applicationId
+            }
+
+            response.failure = { resp, json ->
+                println "Error: Unable to delete application"
+                println "Error: Raw JSON Response: " + json
+
+                System.exit(1)
+            }
+        }
+
+    }
+
+    def deleteApplication(applicationId) {
+
+        def url = baseUrl + 'api/mam/apps/internal/' + applicationId
+
+        HTTPBuilder http = new HTTPBuilder(url)
+        http.request(DELETE) {
+
+            headers.'aw-tenant-code' = authToken
+            headers.'Authorization' = basicAuth
+
+            requestContentType = ContentType.JSON
+
+            response.success = { resp, json ->
+                println "Success: Deleted application: " + applicationId
+            }
+
+            response.failure = { resp, json ->
+                println "Error: Unable to delete application"
+                println "Error: Raw JSON Response: " + json
+
+                System.exit(1)
+            }
+        }
+
+    }
+
+}
+
+final airTool = new AirPluginTool(args[0], args[1])
+final props = airTool.getStepProperties()
+
+def baseUrl = props["baseUrl"]
+def authToken = props["authToken"]
+def userName = props["userName"]
+def password = props["password"]
+def smartGroup = props["smartGroup"]
+def filePath = props["filePath"]
+def applicationName = props["applicationName"]
+
+AirWatchClient client = new AirWatchClient(baseUrl,authToken,userName,password)
+client.uploadIpa(filePath, applicationName, smartGroup)
